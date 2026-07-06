ddee06b6081dd90444ebef8ea3ed44a3d7906ebe
Benjamin Philip Collins <benjamin.p.collins@icloud.com>
2026-07-06 11:40:35 -0400

    Added zred_to_agebins and get_zred to the fitutils.py file.

--- BlueJay/fitutils.py
+++ BlueJay/fitutils.py
@@ -34,6 +34,38 @@ from sedpy import observate
 from scipy.stats import gaussian_kde
 from scipy.signal import find_peaks
 
+
+# Redshift
+def get_zred(galaxy_id):    
+    # --- Read Blue Jay catalogue ---
+    blue = "/Users/benjamincollins/University/Master/BlueJay/BlueJay_sample.txt"
+    tbl = Table.read(blue, format="ascii.basic")
+    
+    row = tbl[tbl['id'] == int(galaxy_id)]
+    
+    # Make sure that the code doesn't crash if it can't find the ID in the catalogue
+    if len(row) == 0:   
+        return None, False
+    
+    z_spec = row['z_spec'][0]
+    
+    if z_spec is not None and not np.isnan(z_spec):
+        return z_spec, True
+    else:
+        z_phot = row['z_phot'][0]
+        return z_phot, False
+
+# Star formation histories
+def zred_to_agebins(zred, z_limit_sfh=20.0, nbins_sfh=8):
+    tuniv = cosmo.age(zred).value*1e9   # Age of the universe at the observed redshift in years
+    #tbinmax = tuniv-cosmo.age(z_limit_sfh).value*1e9 # Maximum age bin edge corresponding to z_limit_sfh
+    tbinmax = tuniv*0.95
+    # Compute edges in logarithmic space
+    log_edges = np.append(np.array([0.0, 6.7, 7.0]), np.linspace(7.0, np.log10(tbinmax), int(nbins_sfh-1))[1:])
+    bin_edges = 10**log_edges   # Convert back to linear space
+    bin_edges /= 1e6 # ensure that edges are in Myr for Bagpipes
+    return bin_edges.tolist()   # return list of age bin edges
+
 def load_prospector_results(galaxy_id, prosp_dir):
     """Function to load Prospector result .h5 files and disect its 
     data structure to be used for easy plotting.
